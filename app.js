bc9944f698134c99497e34f5d2d65673951d690d
Kacper <kacperkapitrebacz@gmail.com>
2023-04-23 21:22:18 +0200

    Now after 25min for work, timer starts count 5 min for break

--- app.js
+++ app.js
@@ -1,38 +1,61 @@
 const display = document.querySelector('.timer-display')
+const message = document.querySelector('.timer-message')
 const startBtn = document.querySelector('.timer-start')
 const pauseBtn = document.querySelector('.timer-pause')
 const resetBtn = document.querySelector('.timer-reset')
 
-let seconds = 1500
-let timer
+const workTime = 25 * 60 // 25 minutes in seconds
+const breakTime = 5 * 60 // 5 minutes in seconds
 
-function updateDisplay() {
-  const minutes = Math.floor(seconds / 60)
-  const remainderSeconds = seconds % 60
-  const displayText = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
-  display.textContent = displayText
-}
+let timerInterval
+let remainingTime = workTime
+let isWorking = true // true for work, false for break
 
 function startTimer() {
-  timer = setInterval(() => {
-    seconds--
-    updateDisplay()
-    if (seconds === 0) {
-      clearInterval(timer)
-    }
-  }, 1000)
+  timerInterval = setInterval(updateTimer, 1000)
 }
 
 function pauseTimer() {
-  clearInterval(timer)
+  clearInterval(timerInterval)
 }
 
 function resetTimer() {
-  clearInterval(timer)
-  seconds = 1500
-  updateDisplay()
+  clearInterval(timerInterval)
+  remainingTime = workTime
+  isWorking = true
+  updateInfo()
+  updateTimer()
 }
 
-startBtn.addEventListener('click', startTimer)
-pauseBtn.addEventListener('click', pauseTimer)
-resetBtn.addEventListener('click', resetTimer)
+function updateTimer() {
+  remainingTime--
+  updateTimerDisplay()
+  if (remainingTime <= 0) {
+    switchTimer()
+  }
+}
+
+function updateTimerDisplay() {
+  const minutes = Math.floor(remainingTime / 60)
+  const seconds = remainingTime % 60
+  const minutesDisplay = minutes < 10 ? `0${minutes}` : minutes
+  const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds
+  display.textContent = `${minutesDisplay}:${secondsDisplay}`
+}
+
+function switchTimer() {
+  isWorking = !isWorking
+  remainingTime = isWorking ? workTime : breakTime
+  updateInfo()
+  updateTimerDisplay()
+}
+
+function updateInfo() {
+  message.textContent = isWorking ? "Work" : "Break"
+}
+
+startBtn.addEventListener("click", startTimer)
+pauseBtn.addEventListener("click", pauseTimer)
+resetBtn.addEventListener("click", resetTimer)
+
+resetTimer()
